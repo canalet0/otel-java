18c64975108cc2fcac56a9b43663e41b49dd5172
canalet0 <roc0canal@gmail.com>
2023-11-07 17:58:56 -0300

    Init project

---
+++ src/main/java/com/example/oteljava/OtelJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.oteljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtelJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oteljava/OtelJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oteljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
