b721f5b214ed8a2ae2e474c7b76b5841ce5b3f73
canalet0 <roc0canal@gmail.com>
2023-11-08 15:40:03 -0300

    Added endpoint to comunicate with webhook

--- src/main/java/com/example/oteljava/controller/AppController.java
+++ src/main/java/com/example/oteljava/controller/AppController.java
@@ -1,18 +1,26 @@
 package com.example.oteljava.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.oteljava.service.integration.IntegrationService;
 import com.example.oteljava.service.integration.NodeServiceIntegration;
+import com.example.oteljava.service.integration.WebhookService;
 
 @RestController
 public class AppController {
 
-    private final NodeServiceIntegration nodeServiceIntegration;
+    protected static final Logger LOGGER = LoggerFactory.getLogger(IntegrationService.class);
 
-    public AppController(NodeServiceIntegration nodeServiceIntegration) {
+    private final NodeServiceIntegration nodeServiceIntegration;
+    private final WebhookService webhookService;
+
+    public AppController(NodeServiceIntegration nodeServiceIntegration, WebhookService webhookService) {
         this.nodeServiceIntegration = nodeServiceIntegration;
+        this.webhookService = webhookService;
     }
 
     @GetMapping
@@ -20,9 +28,18 @@ public class AppController {
         return ResponseEntity.ok("hello");
     }
 
-    @GetMapping("/dice")
+    @GetMapping(name = "GET dice", path = "/dice")
     public ResponseEntity dice() {
+        LOGGER.info("GET dice - Received request");
         return nodeServiceIntegration.rolldice()
+                .map(ResponseEntity::ok)
+                .orElseGet(ResponseEntity.notFound()::build);
+    }
+
+    @GetMapping(name="test", path = "/test")
+    public ResponseEntity test() {
+        LOGGER.info("GET test - Received request");
+        return webhookService.test()
                 .map(ResponseEntity::ok)
                 .orElseGet(ResponseEntity.notFound()::build);
     }
